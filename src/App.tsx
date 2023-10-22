b32a1311ef905c6bb93aebb789440af6e43ed7b4
ahmad safari <sargashte118@gmail.com>
2023-10-22 15:23:12 +0330

    refactor: add log on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { Suspense, useMemo, useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import routesConfig from "@src/routes/routesConfig";
-import { UserContext } from "@context/user/userContext";
-import { IUser } from "./services/users/types";
+import { Suspense, useMemo, useState } from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import routesConfig from "@src/routes/routesConfig"
+import { UserContext } from "@context/user/userContext"
+import { IUser } from "./services/users/types"
 
-const router = createBrowserRouter(routesConfig);
+const router = createBrowserRouter(routesConfig)
 
 function App() {
-  const [user, setUser] = useState<IUser | null>(null);
+  console.log("start app")
+  const [user, setUser] = useState<IUser | null>(null)
 
-  const userValue = useMemo(() => ({ user, setUser }), [user]);
+  const userValue = useMemo(() => ({ user, setUser }), [user])
   return (
     <UserContext.Provider value={userValue}>
-      <div dir="rtl">
+      <div dir='rtl'>
         <Suspense>
           <RouterProvider router={router} />
         </Suspense>
         <ToastContainer rtl style={{ direction: "rtl", fontSize: 20 }} />
       </div>
     </UserContext.Provider>
-  );
+  )
 }
 
-export default App;
+export default App
